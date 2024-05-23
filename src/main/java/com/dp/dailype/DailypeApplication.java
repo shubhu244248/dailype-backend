f43fcf66b6062d06518288e0ed85e8d31aeb60d9
Shubham Ramesh Patil <68527584+shubhu244248@users.noreply.github.com>
2024-05-23 17:22:40 +0530

    project folder commit with all springboot dependecy

---
+++ src/main/java/com/dp/dailype/DailypeApplication.java
@@ -0,0 +1,13 @@
+package com.dp.dailype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dp/dailype/DailypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp.dailype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
